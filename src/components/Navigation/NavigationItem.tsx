0a237bdb442cb7f90ab0d58e51f37c5592c47daf
gil_gil <robert.gil@webeelabs.com.mt>
2023-03-12 13:13:17 +0800

    minor fixes

--- src/components/Navigation/NavigationItem.tsx
+++ src/components/Navigation/NavigationItem.tsx
@@ -16,58 +16,51 @@ export function NavigationItem({
   const { asPath } = useRouter();
   const isActive = asPath === href || asPath === as;
 
+  const styles = isButton
+    ? `
+    bg-transparent
+    font-semibold 
+    py-2 
+    px-4 
+    border 
+    text-primary-500
+    transition-colors
+    border-primary-500 
+    hover:text-primary-700
+    hover:border-primary-700
+    rounded
+  `
+    : `
+    inline-flex
+    items-center
+    text-sm
+    xl:text-base
+    font-normal
+    text-neutral-700
+    py-2
+    px-4
+    xl:px-5
+    hover:text-primary-500
+    transition-colors
+  `;
+
   return (
     <li className="menu-item">
-      {isButton ? (
-        <a
-          href={`#${href}`}
-          className={`
-          bg-transparent
-          font-semibold 
-          py-2 
-          px-4 
-          border 
-          text-primary-500
-          transition-colors
-          border-primary-500 
-          hover:text-primary-700
-          hover:border-primary-700
-          rounded
-          `}
-        >
-          {label}
-        </a>
-      ) : (
-        <a
-          href={`#${href}`}
-          className={`
-              inline-flex
-              items-center
-              text-sm
-              xl:text-base
-              font-normal
-              text-neutral-700
-              py-2
-              px-4
-              xl:px-5
-              hover:text-primary-500
-              transition-colors
-              ease-in-out
-              duration-200
-              ${
-                !isActive
-                  ? ""
-                  : `
+      <a
+        href={`#${href}`}
+        className={`${styles}${
+          !isActive
+            ? ""
+            : `
                 !font-semibold
                 !text-neutral-900
                 bg-neutral-100
               `
-              }
+        }
             `}
-        >
-          {label}
-        </a>
-      )}
+      >
+        {label}
+      </a>
     </li>
   );
 }
